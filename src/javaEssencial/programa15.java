1eb8e4f21050095a65a3308dbb8acb44f5cd55d0
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-05-19 15:40:26 -0300

    Update programa15.java

--- src/javaEssencial/programa15.java
+++ src/javaEssencial/programa15.java
@@ -5,6 +5,7 @@ public class programa15 {
 		int num1 = 5;
 		int num2 = 9;
 		int res;
+		float res2;
 		
 		//soma
 		res = num1 + num2;
@@ -14,6 +15,22 @@ public class programa15 {
 		res = num1 - num2;
 		System.out.println("A subtracao de " + num1 + " - " + num2 + " é " + res );
 		
+		//multiplicacao
+		res = num1 * num2;
+		System.out.println("A multiplicacao de " + num1 + " * " + num2 + " é " + res );
+		
+		//divisao inteira
+		res = num2/num1;
+		System.out.println("A divisao de " + num1 + " / " + num2 + " é " + res );
+		
+		//divisao real
+		res2 = (float)num2/(float)num1;
+		System.out.println("A divisao de " + num1 + " / " + num2 + " é " + res );
+		
+		//modulo
+		res = num1 % num2;
+		System.out.println("o modulod de " + num1 + " % " + num2 + " é " + res );s
+
 
 	}
 
