2ba51d122c2da0d795f2fdff1c974ef8d465cadf
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-05-25 21:20:34 -0300

    Create Programa23.java

---
+++ src/javaEssencial/Programa23.java
@@ -0,0 +1,19 @@
+package br.com.geek.sessao01;
+//Pacotes
+/* Servem para saber onde o projeto estao
+ * os nomes dos pacotes devem ser em letras minusculas, sem caracteres 
+ * especiais e sem espacos
+ * Por convecao, é ulizado nos nomes de pacotes, o dominio da empresa criadora
+ * do projeto, ao contrario, sendo a ultima parte do nome 
+ * sendo o especificador do pacote.
+ * 
+ * Padrao de projeto
+ * MVC Model - View - Controller
+ * 
+ */
+public class Programa23 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
