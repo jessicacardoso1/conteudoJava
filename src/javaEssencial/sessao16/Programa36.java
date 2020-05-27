25a35ebc94554d8ed55631605d8f1f2c41b9da50
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-05-27 14:20:47 -0300

    Update Programa36.java

--- src/javaEssencial/sessao16/Programa36.java
+++ src/javaEssencial/sessao16/Programa36.java
@@ -3,8 +3,20 @@ package br.com.geek.sessao16;
 public class Programa36 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numeros[] = new int[5];
+		
+		for(int i = 0; i < numeros.length; i++) {
+			numeros[i] = i + 3 * 2;
+		}
+		
+		for(int i = 0; i <= numeros.length; i++) {
+			try { //tente fazer isso
+				System.out.println(numeros[i]);
+			}catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Voce esta tentando acessar uma posicao do array que nao existe");
+			}
+		}
+		
 	}
 
 }
