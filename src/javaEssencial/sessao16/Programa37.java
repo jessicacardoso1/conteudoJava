66c4b132a7f22d3d0f0e75fc146156861cba01e1
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-05-27 14:35:52 -0300

    utilazando try/cacht

---
+++ src/javaEssencial/sessao16/Programa37.java
@@ -0,0 +1,23 @@
+package br.com.geek.sessao16;
+
+import java.util.Scanner;
+
+//
+public class Programa37 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro numero para divisao");
+		int num1 = teclado.nextInt();
+		
+		System.out.println("Informe o segundo número para divisao: ");
+		int num2 = teclado.nextInt();
+		try{
+		System.out.println("A divisao de " + num1 + "por" + num2 + "eh" + num1 / num2);
+		}catch(ArithmeticException e) {
+			System.out.println("Nao eh possivel dividir " + num1 + "por " + num2 );
+		}
+		teclado.close();
+	}
+	
+}
