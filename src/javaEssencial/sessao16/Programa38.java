f13a02ed188ee2a5efd423653344ca4f48196be4
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-05-28 16:51:56 -0300

    Update Programa38.java

--- src/javaEssencial/sessao16/Programa38.java
+++ src/javaEssencial/sessao16/Programa38.java
@@ -1,10 +1,33 @@
 package br.com.geek.sessao16;
 
+import java.util.Scanner;
+
 public class Programa38 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Informe o n1");
+		int n1 = teclado.nextInt();
+		
+		System.out.println("Informe o n2");
+		int n2 = teclado.nextInt();
+	
+		try {
+			System.out.println(divisao(n1, n2));
+		}catch(Exception e) {
+			System.out.println("nao foi possivel executar a divisao.");
+		}
+		
+		
+		
+		teclado.close();
+	}
+	
+	//criando uma funcao, que avisa ue tem possibilidade
+	//de lancar uma excecao do tipo Exception
+	public static int divisao(int n1, int n2)  throws Exception{
+		return n1 / n2;
 	}
 
 }
