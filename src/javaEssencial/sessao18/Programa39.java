1e531b064b9bd8ce17a2c6541e349887269632f2
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-06-01 21:45:56 -0300

    Update Programa39.java

--- src/javaEssencial/sessao18/Programa39.java
+++ src/javaEssencial/sessao18/Programa39.java
@@ -17,6 +17,8 @@ public class Programa39 {
 		Conta conta1 = new Conta(1, 111, 1111, cli1);
 		Conta conta2 = new Conta(1, 111, 1111, cli2);
 	
+		Caixa prateleira = new Caixa();
+		
 		System.out.println(cli1); // Por padrao a instancia imprime com toString()
 		
 		if (conta1.equals(conta2)) {
@@ -24,6 +26,24 @@ public class Programa39 {
 		}else {
 			System.out.println("Contas diferentes");
 		}
+		
+		prateleira.adicionar(conta1); // 0 - tem getSaldo
+		prateleira.adicionar(conta2); //1 - n tem getSaldo
+		
+		//Conta conta = prateleira.pegar(0);
+		
+		System.out.println(((Conta)prateleira.pegar(0)).getSaldo()); //Cast
+		
+		prateleira.adicionar(cli1); // 2 = n tem getSaldo();
+		
+		System.out.println(((Cliente)prateleira.pegar(2)).getNome()); //Cast
+
+		if (conta1 instanceof Conta) {
+			System.out.println("O objeto é do tipo conta");	
+		}else {
+			System.out.println("O objwto nao é do tipo conta");
+		}
+
 	}
 
 }
