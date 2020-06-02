3be39452af4cc8858e2567178cdad5382c630457
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-06-02 09:20:45 -0300

    Utilizaçao da bibilioteca Math.

---
+++ src/javaEssencial/sessao18/Programa40.java
@@ -0,0 +1,44 @@
+package br.com.geek.sessao18;
+//MATH
+
+
+public class Programa40 {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.sin(45));
+		
+		System.out.println(Math.cos(45));
+		
+		System.out.println(Math.sin(45));
+		
+		System.out.println(Math.tan(45));
+		
+		System.out.println(Math.round(Math.PI)); //arredonda p menos
+		
+		System.out.println(Math.ceil(Math.PI)); //arredonda p menos
+		
+		System.out.println(Math.floor(Math.PI)); 
+		
+		System.out.println(Math.pow(3,3));
+		
+		System.out.println(Math.round(Math.pow(3,3)));
+		
+		System.out.println(Math.floor(Math.pow(3,3)));
+		
+		System.out.println(Math.sqrt(9));
+		
+		System.out.println(Math.round(Math.sqrt(9)));
+
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
