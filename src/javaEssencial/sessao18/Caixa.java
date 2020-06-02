8c177c2670d65afc54b3b52039cfbf2aee89f173
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-06-01 21:05:22 -0300

    Criaçao dos metodos Depositar e Pegar.

--- src/javaEssencial/sessao18/Caixa.java
+++ src/javaEssencial/sessao18/Caixa.java
@@ -9,5 +9,20 @@ import br.com.geek.sessao11.Conta;
 public class Caixa {
 	private Conta[] contas; //Criando um array de contas
 	private int posicaoLivre;
+	
+	public Caixa() {
+		contas = new Conta[100]; // até 100 contas.
+		posicaoLivre = 0;
+	}
+	
+	private void adicionar(Conta nova) {
+		contas[posicaoLivre] = nova;
+		posicaoLivre++;
+
+	}
+	
+	private Conta pegar(int posicaoInformada) {
+		return this.contas[posicaoInformada];
+	}
 
 }
