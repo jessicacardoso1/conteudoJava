cf6ff0e0edd5f1f36cd3f7cbc558f3bc5424cfd6
Jéssica Cardoso <jessica.cardosor@gmail.com>
2020-06-16 18:45:37 -0300

    Implementaçao de conjuntos do java.

---
+++ src/javaEssencial/sessao21/Programa65.java
@@ -0,0 +1,26 @@
+package br.com.geek.sessao21;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Implementaçao de conjuntos do java
+public class Programa65 {
+	public static void main(String[] args) {
+		Set<String> nomes = new HashSet<String>();
+		
+		nomes.add("Anglina");
+		nomes.add("Ana");
+		nomes.add("Gabriela");
+		
+		
+		String nome1 = "Anglina";
+		String nome2 = "Ana";
+		String nome3 = "Gabriela";
+		
+		System.out.println(nome1);
+		System.out.println(nome2);
+		System.out.println(nome3);
+		
+	}
+
+}
